7ba545df2abba0a1b87c53db7470e45e021743d9
Cannigor <74710282+Cannigor@users.noreply.github.com>
2021-10-10 23:43:18 +0300

    Update circle_test.py

--- circle_test.py
+++ circle_test.py
@@ -44,7 +44,7 @@ while camera.isOpened():
         avg_up = 255  # белый
         if timer_up and time_first is not None:
             time_st = datetime.now() - time_first
-            milliseconds = time_st.seconds * 1000000 + time_st.microseconds / 1000
+            milliseconds = (time_st.seconds * 1000000 + time_st.microseconds) / 1000
             time_current = round(milliseconds)
             print(f'Время смены черного на белый пиксель: {time_current} миллисекунд')
             time_change = time_current
